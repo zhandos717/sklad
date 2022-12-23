2b9017b13643342cde0b04cc24687c21c0c85047
Zhandos <zhandos.zhandarbekov@gmail.com>
2022-12-23 23:23:20 +0600

    added deplay.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -a
+source .env
+set +a
+
+export cmd="cd /var/www/sklad/
+git checkout -- .
+git pull
+"
+
+expect << 'END_EXPECT'
+    spawn ssh $env(USER)@$env(HOST) $env(cmd)
+    expect "*assword*"
+    send $env(PASSWORD)\r
+    expect eof
+END_EXPECT
