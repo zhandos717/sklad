725cc2b1aea4f52c23df5f1246af40d138e924e8
Zhandos <zhandos.zhandarbekov@gmail.com>
2022-12-23 23:38:48 +0600

    added log iframe

--- app/Http/Controllers/Moysklad/IframeController.php
+++ app/Http/Controllers/Moysklad/IframeController.php
@@ -4,15 +4,17 @@ namespace App\Http\Controllers\Moysklad;
 
 use App\Http\Controllers\Controller;
 use App\Services\VendorService;
+use Illuminate\Http\Request;
 
 class IframeController extends Controller
 {
 
-    public function index(\Request $request,VendorService $vendorService)
+    public function index(Request $request,VendorService $vendorService)
     {
 
-        info((string)$request->all());
+        info($request->all());
 
+        dd(1);
 //        $contextKey = $request->get('contextKey');
 //
 //        $employee = vendorApi()->context($contextKey);
