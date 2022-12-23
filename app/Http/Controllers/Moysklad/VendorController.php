cba1d401796ce017b905432bce0d7d02b09b01c3
Zhandos <zhandos.zhandarbekov@gmail.com>
2022-12-24 01:37:04 +0600

    added log iframe

--- app/Http/Controllers/Moysklad/VendorController.php
+++ app/Http/Controllers/Moysklad/VendorController.php
@@ -15,12 +15,10 @@ class VendorController extends Controller
     {
         info('data', [
             'access_token' => $request->get('access.access_token'),
-            'appUid' => $request->get('appUid')
+            'appUid' => $request->get('appUid'),
+            'method' => $request->method()
         ]);
 
-
-        //[2022-12-23 18:00:05] local.INFO: info
-        // {"url":"/vendor-endpoint/api/moysklad/vendor/1.0/apps/7b9b82f5-4f27-4e25-bc55-6f8717ff66c7/e0be3639-7d4c-11ed-0a80-07f300006563",
         //"input":{"appUid":"tis.turmukhambetov","accountName":"zhan96",
         //"access":[
         //{"resource":"https://online.moysklad.ru/api/remap/1.2/",
@@ -28,7 +26,7 @@ class VendorController extends Controller
         //"access_token":"141d7f6c598ca22db658bf4e23a581e42d2b79f9"}],
         //"cause":"Install"}}
 
-        info('end', request()->all());
+//        info('end', request()->all());
 
         return view('descriptor-xml');
     }
