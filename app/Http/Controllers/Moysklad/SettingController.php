a3a82ff9756cf673e5af9f206ccc314736eafce5
Zhandos <zhandos.zhandarbekov@gmail.com>
2022-12-26 23:54:07 +0600

    update AppServiceProvider

--- app/Http/Controllers/Moysklad/SettingController.php
+++ app/Http/Controllers/Moysklad/SettingController.php
@@ -13,9 +13,6 @@ class SettingController extends Controller
     public function updateSettings(UpdateSettingsRequest $request, VendorService $vendorService)
     {
 
-        dump(asset('/'));
-        dd($request->getAccessToken());
-
         $moySklad = MoySkladConfig::updateOrCreate(
             [
                 'access_token' => isset($request->get('access')[0]['access_token']) ? $request->get(
