7fb6f07f0f84909a4f26e09584f1ece9b789b58f
Zhandos <zhandos.zhandarbekov@gmail.com>
2023-01-04 01:23:53 +0600

    update cashbox blade

--- resources/views/moysklad/widgets/cashbox.blade.php
+++ resources/views/moysklad/widgets/cashbox.blade.php
@@ -54,57 +54,58 @@
                 $('#object-new').hide();
                 $('#object').show();
             }
-
-            $('.button--success').click(function () {
-                const form = $(this).parents('form');
-                const button = $(this);
-                const buttonId = Number(button.attr('value'));
-
-                console.log('2121')
-
-                if (button.hasClass('require-popup')) {
-                    const sendingMessage = {
-                        "name": "ShowPopupRequest",
-                        "messageId": buttonId,
-                        "popupName": "formsPopup",
-                        "popupParameters": {
-                            "token": token,
-                            "uid": uid,
-                            "accountId": accountId,
-                            "entity": entity,
-                            "objectId": objectId,
-                            "buttonId": buttonId,
-                        }
-                    };
-                    logSendingMessage(sendingMessage);
-                    hostWindow.postMessage(sendingMessage, '*');
-                    return false;
-                }
-
-                $('#object').hide();
-                $('#doing-action-name').text(button.text());
-                $('#doing-popup').show();
-
-                $.ajax({
-                    type: 'POST',
-                    url: form.attr('action'),
-                    data: {'buttonId': button.attr('value')},
-                    success: function (data, textStatus, jqXHR) {
-                        $('#doing-popup').hide();
-                        $('#object').show();
-                        $('#object-new').html(data);
-                    },
-                    error: function (jqXHR, textStatus, errorThrown) {
-                        $('#doing-popup').hide();
-                        $('#result-error').show();
-                    }
-                });
-
-                return false;
-            });
         }
 
         prepareButtons();
+
+        $('.button--success').click(function () {
+            const form = $(this).parents('form');
+            const button = $(this);
+            const buttonId = Number(button.attr('value'));
+
+            console.log('2121')
+
+            if (button.hasClass('require-popup')) {
+                const sendingMessage = {
+                    "name": "ShowPopupRequest",
+                    "messageId": buttonId,
+                    "popupName": "formsPopup",
+                    "popupParameters": {
+                        "token": token,
+                        "uid": uid,
+                        "accountId": accountId,
+                        "entity": entity,
+                        "objectId": objectId,
+                        "buttonId": buttonId,
+                    }
+                };
+                logSendingMessage(sendingMessage);
+                hostWindow.postMessage(sendingMessage, '*');
+                return false;
+            }
+
+            $('#object').hide();
+            $('#doing-action-name').text(button.text());
+            $('#doing-popup').show();
+
+            $.ajax({
+                type: 'POST',
+                url: form.attr('action'),
+                data: {'buttonId': button.attr('value')},
+                success: function (data, textStatus, jqXHR) {
+                    $('#doing-popup').hide();
+                    $('#object').show();
+                    $('#object-new').html(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    $('#doing-popup').hide();
+                    $('#result-error').show();
+                }
+            });
+
+            return false;
+        });
+
     </script>
 @endsection
 @section('content')
