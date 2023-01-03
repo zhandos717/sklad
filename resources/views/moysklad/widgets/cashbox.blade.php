6e84311e7c2743f7b07e1f4534de4e079e0808d3
Zhandos <zhandos.zhandarbekov@gmail.com>
2023-01-04 01:16:40 +0600

    update cashbox blade

--- resources/views/moysklad/widgets/cashbox.blade.php
+++ resources/views/moysklad/widgets/cashbox.blade.php
@@ -2,58 +2,31 @@
 @section('title', 'Popup')
 @section('js')
     <script>
-        const hostWindow = window.parent;
-        const token = "7c4d83ee4b3ce4fa922a1ac9292f59799a7f1a2a";
-        const uid = "admin@zhan96";
-        const accountId = "e0be3639-7d4c-11ed-0a80-07f300006563";
-        const entity = "customerorder";
-        let objectId = "";
+        $('.button--success').click(function () {
+            const form = $(this).parents('form');
+            const button = $(this);
+            const buttonId = Number(button.attr('value'));
 
-        window.addEventListener("message", function (event) {
-            const receivedMessage = event.data;
+            console.log('2121')
 
-            logReceivedMessage(receivedMessage);
-
-            if (receivedMessage.name === 'Open' || receivedMessage.name === 'Save') {
-                const oReq = new XMLHttpRequest();
-                oReq.addEventListener("load", function () {
-                    window.document.getElementById("object").innerHTML = this.responseText;
-                    prepareButtons();
-                    const sendingMessage = {
-                        name: "OpenFeedback",
-                        correlationId: receivedMessage.messageId
-                    };
-                    logSendingMessage(sendingMessage);
-                    hostWindow.postMessage(sendingMessage, '*');
-                });
-                objectId = receivedMessage.objectId;
-                oReq.open("GET", '{{ url('/')}}' + '/widgets/get-object?token=' + token + '&uid=' + uid + '&accountId=' + accountId + '&entity=customerorder&objectId=' + receivedMessage.objectId);
-                oReq.send();
-            } else if (receivedMessage.name === 'ShowPopupResponse' && receivedMessage.popupName === 'formsPopup' && receivedMessage.popupResolution === 'normal' && (typeof receivedMessage.popupResponse !== 'undefined')) {
-                clickButton(receivedMessage.correlationId, receivedMessage.popupResponse);
+            if (button.hasClass('require-popup')) {
+                const sendingMessage = {
+                    "name": "ShowPopupRequest",
+                    "messageId": buttonId,
+                    "popupName": "formsPopup",
+                    "popupParameters": {
+                        "token": token,
+                        "uid": uid,
+                        "accountId": accountId,
+                        "entity": entity,
+                        "objectId": objectId,
+                        "buttonId": buttonId,
+                    }
+                };
+                logSendingMessage(sendingMessage);
+                hostWindow.postMessage(sendingMessage, '*');
+                return false;
             }
-        });
-
-        function logReceivedMessage(msg) {
-            logMessage("→ Received", msg)
-        }
-
-        function logSendingMessage(msg) {
-            logMessage("← Sending", msg)
-        }
-
-        function logMessage(prefix, msg) {
-            const messageAsString = JSON.stringify(msg);
-            console.log(prefix + " message: " + messageAsString);
-        }
-
-        function body() {
-            return window.document.body;
-        }
-
-        function clickButton(buttonId, popupFormParameters) {
-            const form = $('#click-form');
-            const button = form.children('button[value=' + buttonId + ']');
 
             $('#object').hide();
             $('#doing-action-name').text(button.text());
@@ -62,7 +35,7 @@
             $.ajax({
                 type: 'POST',
                 url: form.attr('action'),
-                data: {'buttonId': buttonId, 'popupFormParameters': popupFormParameters},
+                data: {'buttonId': button.attr('value')},
                 success: function (data, textStatus, jqXHR) {
                     $('#doing-popup').hide();
                     $('#object').show();
@@ -73,68 +46,9 @@
                     $('#result-error').show();
                 }
             });
-        }
 
-        function prepareButtons() {
-
-            if ($('#result-error').css('display') != 'none') {
-                $('#result-error').hide();
-                $('#object').show();
-            }
-
-            if ($('#object-new').css('display') != 'none') {
-                $('#object-new').hide();
-                $('#object').show();
-            }
-
-            $('.button--success').click(function () {
-                const form = $(this).parents('form');
-                const button = $(this);
-                const buttonId = Number(button.attr('value'));
-
-                console.log('2121')
-
-                if (button.hasClass('require-popup')) {
-                    const sendingMessage = {
-                        "name": "ShowPopupRequest",
-                        "messageId": buttonId,
-                        "popupName": "formsPopup",
-                        "popupParameters": {
-                            "token": token,
-                            "uid": uid,
-                            "accountId": accountId,
-                            "entity": entity,
-                            "objectId": objectId,
-                            "buttonId": buttonId,
-                        }
-                    };
-                    logSendingMessage(sendingMessage);
-                    hostWindow.postMessage(sendingMessage, '*');
-                    return false;
-                }
-
-                $('#object').hide();
-                $('#doing-action-name').text(button.text());
-                $('#doing-popup').show();
-
-                $.ajax({
-                    type: 'POST',
-                    url: form.attr('action'),
-                    data: {'buttonId': button.attr('value')},
-                    success: function (data, textStatus, jqXHR) {
-                        $('#doing-popup').hide();
-                        $('#object').show();
-                        $('#object-new').html(data);
-                    },
-                    error: function (jqXHR, textStatus, errorThrown) {
-                        $('#doing-popup').hide();
-                        $('#result-error').show();
-                    }
-                });
-
-                return false;
-            });
-        }
+            return false;
+        });
     </script>
 @endsection
 @section('content')
