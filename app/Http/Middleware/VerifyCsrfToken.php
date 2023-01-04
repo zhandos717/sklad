2c0b673e1b6481d446b8b34ccd6f9496529bd0ee
Zhandos <zhandos.zhandarbekov@gmail.com>
2023-01-05 00:39:27 +0600

    update cashbox blade

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/vendor-endpoint/api/moysklad/*',
-        '/settings/*'
+        '/settings/*',
+        '/sale'
     ];
 }
