b47ae536cb6257684fbb564b9eabc01a7127655e
Zhandos <zhandos.zhandarbekov@gmail.com>
2023-01-08 23:04:23 +0600

    update cashbox blade

--- app/Http/Controllers/Moysklad/VendorController.php
+++ app/Http/Controllers/Moysklad/VendorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Moysklad;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SettingsRequest;
 use App\Http\Requests\UpdateSettingsRequest;
 use App\Http\Resources\Moysklad\SettingResource;
 use App\Http\Resources\Moysklad\VendorResource;
@@ -34,21 +35,25 @@ class VendorController extends Controller
         ]);
     }
 
-    public function update(UpdateSettingsRequest $request, VendorService $vendorService): SettingResource
+    public function update(SettingsRequest $request, VendorService $vendorService)
     {
-        $moySklad = MoySkladConfig::updateOrCreate(
+        MoySkladConfig::updateOrCreate(
             [
-                'app_id'         => $request->input('appId'),
-                'account_id'     => $request->input('accountId'),
-                'prosklad_token' => $request->input('tis_token'),
+                'app_id'     => config('moysklad.app_id'),
+                'account_id' => $request->input('account_id'),
+                'tis_token'  => $request->input('tis_token'),
             ],
             [
                 'access_token' => isset($request->get('access')[0]['access_token']) ? $request->get(
                     'access'
                 )[0]['access_token'] : null,
                 'status'       => MoySkladConfig::ACTIVATED,
+                'info_message' => $request->input('infoMessage'),
+                'store'        => $request->input('store')
             ],
         );
+
+        $moySklad = MoySkladConfig::whereAccountId($request->input('account_id'))->first();
 
         $vendorService->updateAppStatus(
             config('moysklad.app_id'),
