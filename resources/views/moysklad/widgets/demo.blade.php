347e8c35a6a9b034ee2407767d5ee15e03603dac
Zhandos <zhandos.zhandarbekov@gmail.com>
2023-01-09 02:31:17 +0600

    update MoySkladConfig

--- resources/views/moysklad/widgets/demo.blade.php
+++ resources/views/moysklad/widgets/demo.blade.php
@@ -36,6 +36,9 @@
                 const oReq = new XMLHttpRequest();
                 oReq.addEventListener("load", function () {
                     window.document.getElementById("table").innerHTML = this.responseText;
+
+                    logReceivedMessage(this);
+
                 });
 
                 document.getElementById("objectId").value = receivedMessage.objectId;
