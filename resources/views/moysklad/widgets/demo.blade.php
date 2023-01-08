8f84fd6458ed04706c1d7d7c8d6c408a6aa34062
Zhandos <zhandos.zhandarbekov@gmail.com>
2023-01-09 02:29:05 +0600

    update MoySkladConfig

--- resources/views/moysklad/widgets/demo.blade.php
+++ resources/views/moysklad/widgets/demo.blade.php
@@ -3,6 +3,10 @@
 
 @section('styles')
     <style>
+        #fiscal-receipt {
+            display: none;
+        }
+
         @media print {
             #fiscal-receipt {
                 display: block;
@@ -31,8 +35,10 @@
             if (receivedMessage.name === 'Open') {
                 const oReq = new XMLHttpRequest();
                 oReq.addEventListener("load", function () {
-                    window.document.getElementById("content").innerHTML = this.responseText;
+                    window.document.getElementById("table").innerHTML = this.responseText;
                 });
+
+                document.getElementById("objectId").value = receivedMessage.objectId;
 
                 oReq.open("GET", '/widgets/get-item?accountId={{$accountId??''}}&entity={{$entity}}&objectId=' + receivedMessage.objectId);
                 oReq.send();
@@ -45,7 +51,7 @@
                     logSendingMessage(sendingMessage);
                     hostWindow.postMessage(sendingMessage, '*');
 
-                }, 100);
+                }, 200);
             }
         });
 
@@ -65,11 +71,59 @@
         function body() {
             return window.document.body;
         }
+
+
+        function send(e, form) {
+            document.querySelector('#doing-popup').classList.toggle('hidden')
+            fetch(form.action, {method: 'post', body: new FormData(form)})
+                .then((response) => {
+                    return response.json();
+                })
+                .then((result) => {
+                    console.log(result);
+                    document.querySelector('#fiscal-receipt').innerHTML = result.data.view
+                    window.print();
+                    document.querySelector('#doing-popup').classList.toggle('hidden')
+                    document.querySelector('.alert-success').classList.toggle('hidden')
+                    document.querySelector('.button').classList.toggle('hidden')
+                });
+            e.preventDefault();
+        }
     </script>
 @endsection
 
 @section('content')
-    <div id="content">
-
+    <div id="table">
     </div>
+    <div id="fiscal-receipt">
+    </div>
+    <div class="alert alert-success hidden">
+        Операция фискализирована!
+    </div>
+    <form method="POST" onsubmit="send(event,this)" action="{{route('sale')}}" id="click-form" method="POST">
+        <label>
+            <input name="uuid" value="{{ \Ramsey\Uuid\Uuid::uuid4()->toString()  }}" hidden="">
+        </label>
+        @csrf
+        <label>
+            <input hidden name="accountId" value="{{$accountId}}">
+        </label>
+        <label>
+            <input hidden name="objectId" id="objectId">
+        </label>
+
+        <div class="hidden" id="doing-popup">
+            <div class="d-flex justify-content-center">
+                <div class="spinner-border" role="status">
+                </div>
+            </div>
+        </div>
+
+        <button class="button button--success" type="submit" value="12">
+            Печать чека
+        </button>
+
+    </form>
 @stop
+
+
