c239d340e42b9404f55fcbc26e1f0ee86079fcb3
Zhandos <zhandos.zhandarbekov@gmail.com>
2023-01-12 01:01:37 +0600

    added orchid admin panel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -55,9 +55,9 @@
                     Bootstrap</h2>
                 <div data-aos="fade-up" data-aos-delay="600">
                     <div class="text-center text-lg-start">
-                        <a href="#about"
+                        <a href="#go"
                            class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
-                            <span>Get Started</span>
+                            <span>Начать</span>
                             <i class="bi bi-arrow-right"></i>
                         </a>
                     </div>
