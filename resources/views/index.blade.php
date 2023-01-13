42864dc89035c1dfa536042af8cbd2b41e7f87bb
zhandos <zh.zhandarbekov@wiponapp.com>
2023-01-13 15:18:04 +0600

    update index blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,21 +26,18 @@
 <!-- ======= Header ======= -->
 <header id="header" class="header fixed-top">
     <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
-
         <a href="/" class="logo d-flex align-items-center">
             <img src="{{asset('assets/img/logo.png')}}" alt="">
             <span>TIS KZ</span>
         </a>
-
         <nav id="navbar" class="navbar">
             <ul>
                 <li><a class="nav-link scrollto active" href="#hero">Главная</a></li>
-                <li><a class="nav-link scrollto" href="#about">О нас</a></li>
+                {{--                <li><a class="nav-link scrollto" href="#about">О нас</a></li>--}}
                 <li><a class="nav-link scrollto" href="#contact">Контакты</a></li>
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
         </nav><!-- .navbar -->
-
     </div>
 </header><!-- End Header -->
 
