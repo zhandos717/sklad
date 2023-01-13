09651f9a00edd03e9521276c5d0e7a2f81bfc908
zhandos <zh.zhandarbekov@wiponapp.com>
2023-01-13 12:25:29 +0600

    update index blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -46,7 +46,6 @@
 
 <!-- ======= Hero Section ======= -->
 <section id="hero" class="hero d-flex align-items-center">
-
     <div class="container">
         <div class="row">
             <div class="col-lg-6 d-flex flex-column justify-content-center">
@@ -68,6 +67,61 @@
             </div>
         </div>
     </div>
+    <main id="main">
+        <section id="contact" class="contact">
+            <div class="container aos-init aos-animate" data-aos="fade-up">
+                <header class="section-header"><h2>Контакты</h2>
+                    <p>Свяжитесь с нами</p></header>
+                <div class="row gy-4">
+                    {{--                    <div class="col-lg-6">--}}
+                    {{--                        <div class="row gy-4">--}}
+                    {{--                            <div class="col-md-6">--}}
+                    {{--                                <div class="info-box"><i class="bi bi-geo-alt"></i>--}}
+                    {{--                                    <h3>Address</h3>--}}
+                    {{--                                    <p>A108 Adam Street,<br>New York, NY 535022</p></div>--}}
+                    {{--                            </div>--}}
+                    {{--                            <div class="col-md-6">--}}
+                    {{--                                <div class="info-box"><i class="bi bi-telephone"></i>--}}
+                    {{--                                    <h3>Call Us</h3>--}}
+                    {{--                                    <p>+1 5589 55488 55<br>+1 6678 254445 41</p></div>--}}
+                    {{--                            </div>--}}
+                    {{--                            <div class="col-md-6">--}}
+                    {{--                                <div class="info-box"><i class="bi bi-envelope"></i>--}}
+                    {{--                                    <h3>Email Us</h3>--}}
+                    {{--                                    <p>info@example.com<br>contact@example.com</p></div>--}}
+                    {{--                            </div>--}}
+                    {{--                            <div class="col-md-6">--}}
+                    {{--                                <div class="info-box"><i class="bi bi-clock"></i>--}}
+                    {{--                                    <h3>Open Hours</h3>--}}
+                    {{--                                    <p>Monday - Friday<br>9:00AM - 05:00PM</p></div>--}}
+                    {{--                            </div>--}}
+                    {{--                        </div>--}}
+                    {{--                    </div>--}}
+                    <div class="col-lg-6">
+                        <form action="forms/contact" method="post" class="php-email-form">
+                            <div class="row gy-4">
+                                @csrf
+                                <div class="col-md-6"><input type="text" name="name" class="form-control"
+                                                             placeholder="Ваше имя" required=""></div>
+                                <div class="col-md-6 "><input type="email" class="form-control" name="email"
+                                                              placeholder="Почта" required=""></div>
+                                <div class="col-md-12"><input type="tel" class="form-control" name="phone"
+                                                              placeholder="Телефон" required=""></div>
+                                <div class="col-md-12"><textarea class="form-control" name="message" rows="6"
+                                                                 placeholder="Сообщение" required=""></textarea></div>
+                                <div class="col-md-12 text-center">
+                                    <div class="loading">Загрузка</div>
+                                    <div class="error-message"></div>
+                                    <div class="sent-message">Ваше сообщение отправлено, спасибо!</div>
+                                    <button type="submit">Отправить сообщение</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 
 </section><!-- End Hero -->
 <script src="{{asset('assets/vendor/purecounter/purecounter_vanilla.js')}}"></script>
