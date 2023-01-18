379d2d2ae4fa55819c7edf4f6ac623225152e956
zhandos <zh.zhandarbekov@wiponapp.com>
2023-01-18 10:26:33 +0600

    update index blade

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -115,13 +115,12 @@
                             В заказах покупателей видим виджет "Интеграция с ТИС для Казахстана"
                         </div>
                         <div class="d-flex align-items-center mb-2"><i class="bi bi-check2"></i><h4></h4>
-                           Нажмите "Выбить чек"
+                            Нажмите "Выбить чек"
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-6"><img src="{{asset('assets/img/features-2.png')}}" class="img-fluid" alt=""></div>
             </div>
-        </div>
     </section>
 
     <section id="contact" class="contact">
