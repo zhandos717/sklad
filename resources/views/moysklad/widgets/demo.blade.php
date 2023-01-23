edbe38e68fbaf399ad5b0f3198a1db6b574a1ad5
Zhandos <zhandos.zhandarbekov@gmail.com>
2023-01-24 01:03:46 +0600

    update demo blade

--- resources/views/moysklad/widgets/demo.blade.php
+++ resources/views/moysklad/widgets/demo.blade.php
@@ -33,11 +33,6 @@
             logReceivedMessage(receivedMessage);
 
             if (receivedMessage.name === 'Open' || receivedMessage.name === 'Save') {
-                const oReq = new XMLHttpRequest();
-                oReq.addEventListener("load", function () {
-                    window.document.getElementById("table").innerHTML = this.responseText;
-                });
-
                 var oReq = new XMLHttpRequest();
                 oReq.addEventListener("load", function() {
                     window.document.getElementById("object").innerHTML = this.responseText;
