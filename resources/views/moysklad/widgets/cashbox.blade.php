921047848118f88dcf569865d68fbb8ea5e90939
zhandos <zh.zhandarbekov@wiponapp.com>
2023-01-24 10:13:38 +0600

    update index blade

--- resources/views/moysklad/widgets/cashbox.blade.php
+++ resources/views/moysklad/widgets/cashbox.blade.php
@@ -1,5 +1,31 @@
-@if(isset($sale->fiscal_receipt))
-    <div class="alert alert-success">
-        Чек за данный заказ уже выбит!
-    </div>
-@endif
+<div>
+    <table class="ui-table">
+        <thead>
+        <tr>
+            <th>Наименование</th>
+            <th>Кол-во</th>
+            <th>Цена</th>
+            <th>Сумма</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($items as $item)
+            <tr>
+                <td> {{ $item->product->name  }}</td>
+                <td> {{ $item->quantity  }}</td>
+                <td> {{ $item->price  }}</td>
+                <td> {{ $item->total  }}</td>
+            </tr>
+        @endforeach
+        <tr>
+            <td colspan="4" class="text-center">{{ $items->sum('total')  }} тг.</td>
+        </tr>
+        <tbody>
+    </table>
+    @if(isset($sale->fiscal_receipt))
+        <div class="alert alert-success">
+            Чек за данный заказ уже выбит!
+        </div>
+    @endif
+
+</div>
