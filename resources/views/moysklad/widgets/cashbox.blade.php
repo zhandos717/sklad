3e6786e620f226f215748bf425d01bce07b35f12
zhandos <zh.zhandarbekov@wiponapp.com>
2023-01-24 10:09:59 +0600

    update index blade

--- resources/views/moysklad/widgets/cashbox.blade.php
+++ resources/views/moysklad/widgets/cashbox.blade.php
@@ -3,27 +3,3 @@
         Чек за данный заказ уже выбит!
     </div>
 @endif
-
-<table class="ui-table">
-    <thead>
-    <tr>
-        <th>Наименование</th>
-        <th>Кол-во</th>
-        <th>Цена</th>
-        <th>Сумма</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach($items as $item)
-        <tr>
-            <td> {{ $item->product->name  }}</td>
-            <td> {{ $item->quantity  }}</td>
-            <td> {{ $item->price  }}</td>
-            <td> {{ $item->total  }}</td>
-        </tr>
-    @endforeach
-    <tr>
-        <td colspan="4" class="text-center">{{ $items->sum('total')  }} тг.</td>
-    </tr>
-    <tbody>
-</table>
