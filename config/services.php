98e53fad881131e4ab90944154ef5aededab768b
zhandos717 <64123073+zhandos717@users.noreply.github.com>
2023-01-25 11:37:48 +0600

    update services

--- config/services.php
+++ config/services.php
@@ -30,7 +30,7 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
-    'cashbox'=[
+    'cashbox'=>[
         'url'=>env('CASHBOX_URL')
     ]
 
